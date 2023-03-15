740e5073773460d53740e0e2f309f743c968bf23
Benson Mokogi <b.mokogi@gmail.com>
2023-03-15 14:04:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (num1,num2)=>{   
+    return 20;
+}
+divide(2000,100);
+
+const square = x =>{
+    return (x**2);
+}
+square();
+
+const add = (num3,num4)=>{
+    return num3 + num4;
+}
+add();
